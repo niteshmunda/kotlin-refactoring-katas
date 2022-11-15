18439207f20327245b084318e7b711859a9adc8e
Nitesh_Munda <niteshsingh.m@dunzo.in>
2022-11-15 18:34:49 +0530

    extract constants to file

---
+++ gilded-rose/src/main/kotlin/com/gildedrose/Constants.kt
@@ -0,0 +1,12 @@
+package com.gildedrose
+
+object Constants {
+  const val AGED_BRIE = "Aged Brie"
+  const val BCKS_PASSES = "Backstage passes to a TAFKAL80ETC concert"
+  const val SULFURAS_HAND = "Sulfuras, Hand of Ragnaros"
+  const val ZERO = 0
+  const val QUALITY_BOUND = 50
+  const val ONE = 1
+  const val ELEVEN = 11
+  const val SIX = 6
+}
--- gilded-rose/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ gilded-rose/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,33 +1,39 @@
 package com.gildedrose
 
+import com.gildedrose.Constants.AGED_BRIE
+import com.gildedrose.Constants.BCKS_PASSES
+import com.gildedrose.Constants.ELEVEN
+import com.gildedrose.Constants.ONE
+import com.gildedrose.Constants.QUALITY_BOUND
+import com.gildedrose.Constants.SIX
+import com.gildedrose.Constants.SULFURAS_HAND
+import com.gildedrose.Constants.ZERO
+
 class GildedRose(var items: Array<Item>) {
-  companion object {
-    const val AGED_BRIE = "Aged Brie"
-    const val BCKS_PASSES = "Backstage passes to a TAFKAL80ETC concert"
-    const val SULFURAS_HAND = "Sulfuras, Hand of Ragnaros"
-  }
+
   fun updateQuality() {
     for (i in items.indices) {
+
       if (items[i].name != AGED_BRIE && items[i].name != BCKS_PASSES) {
-        if (items[i].quality > 0) {
+        if (items[i].quality > ZERO) {
           if (items[i].name != SULFURAS_HAND) {
-            items[i].quality = items[i].quality - 1
+            items[i].quality = items[i].quality - ONE
           }
         }
       } else {
-        if (items[i].quality < 50) {
-          items[i].quality = items[i].quality + 1
+        if (items[i].quality < QUALITY_BOUND) {
+          items[i].quality = items[i].quality + ONE
 
           if (items[i].name == BCKS_PASSES) {
-            if (items[i].sellIn < 11) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
+            if (items[i].sellIn < ELEVEN) {
+              if (items[i].quality < QUALITY_BOUND) {
+                items[i].quality = items[i].quality + ONE
               }
             }
 
-            if (items[i].sellIn < 6) {
-              if (items[i].quality < 50) {
-                items[i].quality = items[i].quality + 1
+            if (items[i].sellIn < SIX) {
+              if (items[i].quality < QUALITY_BOUND) {
+                items[i].quality = items[i].quality + ONE
               }
             }
           }
@@ -35,23 +41,23 @@ class GildedRose(var items: Array<Item>) {
       }
 
       if (items[i].name != SULFURAS_HAND) {
-        items[i].sellIn = items[i].sellIn - 1
+        items[i].sellIn = items[i].sellIn - ONE
       }
 
-      if (items[i].sellIn < 0) {
+      if (items[i].sellIn < ZERO) {
         if (items[i].name != AGED_BRIE) {
           if (items[i].name != BCKS_PASSES) {
-            if (items[i].quality > 0) {
+            if (items[i].quality > ZERO) {
               if (items[i].name != SULFURAS_HAND) {
-                items[i].quality = items[i].quality - 1
+                items[i].quality = items[i].quality - ONE
               }
             }
           } else {
             items[i].quality = items[i].quality - items[i].quality
           }
         } else {
-          if (items[i].quality < 50) {
-            items[i].quality = items[i].quality + 1
+          if (items[i].quality < QUALITY_BOUND) {
+            items[i].quality = items[i].quality + ONE
           }
         }
       }
